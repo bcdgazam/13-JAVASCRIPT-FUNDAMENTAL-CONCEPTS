36066bb84d4a758e6d64f46f79c22cf3ced92521
bcdazam <bcdazam@gmail.com>
2024-04-01 18:01:13 +0600

    arry, index, set index, indexOf

---
+++ 13-2.js
@@ -0,0 +1,8 @@
+//13-2 Array, index, set by index, indexOf
+
+
+var friendsAge = [12, 16, 18, 24];
+var habluAge = friendsAge[2];
+friendsAge[1] = 26; //input value to an array
+var position = friendsAge.indexOf(18)//get array index/position
+console.log(position);
