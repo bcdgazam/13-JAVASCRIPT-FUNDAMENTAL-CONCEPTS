a81b302a69e3e872618ce7c4f5540e8897f9fa57
bcdazam <bcdazam@gmail.com>
2024-04-02 10:35:01 +0600

    push, lengh, pop, shift, unshift

---
+++ 13-3.js
@@ -0,0 +1,23 @@
+//13-3 Array advanced, Push, pop, array length
+
+var friendsAge = [12, 14, 17, 18]
+
+//Push
+friendsAge.push(22);
+friendsAge.push(25);
+console.log(friendsAge);
+
+//array length: How much person has in an array .length
+console.log(friendsAge.length);
+
+//Pop: (put out elements from array"end")
+friendsAge.pop()
+console.log(friendsAge)
+
+//shift: put out elements from array"Start")
+friendsAge.shift()
+console.log(friendsAge);
+
+//unshift: put in elements in array"begining")
+friendsAge.unshift(39)
+console.log(friendsAge);
