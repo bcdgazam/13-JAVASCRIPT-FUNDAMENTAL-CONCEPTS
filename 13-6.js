561a0d223a9d25f90e03b7b433d41c4c499f4b9f
bcdazam <bcdazam@gmail.com>
2024-04-11 01:19:38 +0600

    for loop

---
+++ 13-6.js
@@ -0,0 +1,10 @@
+// for(var i = 0; i <= 10; i++){
+// console.log(i);
+// }
+
+var nums = [10, 20, 30, 40, 50, 60];
+
+for(var i = 0; i < nums.length; i++){
+    var elements = nums[i];
+    console.log(elements);
+}
