8000f924e5532a5d59addf57b4f648fc4f29f44e
Golam Azam <bcdazam@gmail.com>
2024-04-17 15:08:42 +0600

    switch case

---
+++ 13-7.js
@@ -0,0 +1,23 @@
+//13-7 Javascript Switch Case Break And Default
+
+num = 5;
+
+switch(num){
+    case 1000:
+        console.log('I am 1000');
+        break;
+    case 100:
+        console.log('I am 100');
+        break;
+    case 50:
+        console.log('I am 50');
+        break;
+    case 20:
+        console.log('I am 20');
+        break;
+    case 10:
+        console.log('I am either 20 or 10');
+        break;
+    default:
+        console.log('I do\'t know who you are')
+}
