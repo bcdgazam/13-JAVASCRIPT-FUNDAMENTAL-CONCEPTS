2b79978d4c54b39df0e48fa35b2d8bc69d45c214
Golam Azam <bcdazam@gmail.com>
2024-04-17 15:23:17 +0600

    function and call

---
+++ 13-8.js
@@ -0,0 +1,12 @@
+//13-8 Function, call function
+
+function fnName() {
+    console.log('hay! How are you?');
+    console.log('I am good, Thank you!');
+}
+
+fnName();
+var abarDekha = 'sokale dekha hole';
+fnName();
+var sondhyDekhaHole = 'sondhay abar dekha hoile';
+fnName();
