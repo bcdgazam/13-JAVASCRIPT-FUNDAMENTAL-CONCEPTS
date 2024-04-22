bb41940e6cc67bf0db950a5239802d25758d07ca
Golam Azam <bcdazam@gmail.com>
2024-04-22 15:43:06 +0600

    practice

--- prac.js
+++ prac.js
@@ -1,6 +1,25 @@
 //Practice file
 
 /*
-
+    13-2 Array, index, set by index, indexOf
+    13-3 Array advanced, Push, pop, array length
+    13-4 array add and remove element from the beginning and slice
+    13-5 While loop, debug javascript code, less or equal
+    13-6 For loop loop, run a loop for each element of an array
+    13-7 Javascript Switch Case Break And Default
+    13-8 Function, call function
+    13-9 Function Parameter, multiple parameter, function return
+    13-10 Comment, multiple lines comment
+    13-11 Object, key value pair, get object property, set value
 */
 
+let arr = [23, 30, 78, 32];
+console.log(arr);
+
+console.log(arr[2]);
+
+arr[1] = 90;
+console.log(arr);
+
+let element = arr.indexOf(32);
+console.log(element);
