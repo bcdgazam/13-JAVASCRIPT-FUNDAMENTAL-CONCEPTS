4ae089e018133b6785482e9a7b9be3cd73caa7a5
Golam Azam <bcdazam@gmail.com>
2024-04-29 14:59:05 +0600

    switch case practice

--- prac.js
+++ prac.js
@@ -24,9 +24,31 @@
 //     num++;
 // }
 
-let forLoop = [10, 20, 30, 40, 50, 60, 70];
+// let forLoop = [10, 20, 30, 40, 50, 60, 70];
 
-for ( let i = 0; i <forLoop.length; i++){
-    let element = forLoop[i];
-    console.log(element);
+// for ( let i = 0; i <forLoop.length; i++){
+//     let element = forLoop[i];
+//     console.log(element);
+// }
+
+//switch case:
+
+let num = 1000;
+switch (num) {
+    case 100:
+        console.log('I am 100')    
+        break;
+    case 200:
+        console.log('I am 200;');
+        break;
+    case 300:
+        console.log('I am 300');
+        break;
+    case 400:
+        console.log('I am 400');
+        break
+
+    default:
+        console.log('Sorry, I don\'t Know')
+        break;
 }
