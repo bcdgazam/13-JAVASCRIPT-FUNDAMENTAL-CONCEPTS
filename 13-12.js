5d8eb30bbeb921b8c6eec5bf7b51af92ca1e4ce7
Golam Azam <bcdazam@gmail.com>
2024-05-04 11:51:51 +0600

    array practice

--- 13-12.js
+++ 13-12.js
@@ -1,2 +1,14 @@
 //13-12 Basic Javascript overview, Module Summary
 
+/*
+    13-2 Array, index, set by index, indexOf
+    13-3 Array advanced, Push, pop, array length (splice , concat)
+    13-4 array add and remove element from the beginning and slice
+    13-5 While loop, debug javascript code, less or equal
+    13-6 For loop loop, run a loop for each element of an array
+    13-7 Javascript Switch Case Break And Default
+    13-8 Function, call function
+    13-9 Function Parameter, multiple parameter, function return
+    13-10 Comment, multiple lines comment
+    13-11 Object, key value pair, get object property, set value
+*/
--- prac.js
+++ prac.js
@@ -1,44 +1,5 @@
-//Practice file
-
-/*
-    13-2 Array, index, set by index, indexOf
-    13-3 Array advanced, Push, pop, array length (splice , concat)
-    13-4 array add and remove element from the beginning and slice
-    13-5 While loop, debug javascript code, less or equal
-    13-6 For loop loop, run a loop for each element of an array
-    13-7 Javascript Switch Case Break And Default
-    13-8 Function, call function
-    13-9 Function Parameter, multiple parameter, function return
-    13-10 Comment, multiple lines comment
-    13-11 Object, key value pair, get object property, set value
-*/
-
-//special
-// let arr = [10, 20, 30, 40, 50, 80, 90];
-// let newArr = arr.slice(0, 3).concat(arr.slice(4));
-// console.log(newArr);
-
-// let num = 10;
-// while (num <= 15){
-//     console.log(num);
-//     num++;
-// }
-
-// let forLoop = [10, 20, 30, 40, 50, 60, 70];
-
-// for ( let i = 0; i <forLoop.length; i++){
-//     let element = forLoop[i];
-//     console.log(element);
-// }
-
-//switch case:
-
-function thisIsTest(){
-    console.log('Hi I am hablu, I dont have a good knowledge');
-    console.log('Are you okay? I hope so!')
-}
-
-thisIsTest();
-thisIsTest();
-thisIsTest();
-thisIsTest();
+let arr = [12, 23, 345, 34, 534, 22, 32, 5]
+arr.push(300);
+arr.pop()
+let newarr = arr.slice(345);
+console.log(newarr);
