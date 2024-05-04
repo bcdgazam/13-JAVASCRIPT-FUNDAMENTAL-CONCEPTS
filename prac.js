168b837eae86760d04e1c1374ab0a9fe74d9e775
Golam Azam <bcdazam@gmail.com>
2024-05-04 12:18:45 +0600

    while practice

--- prac.js
+++ prac.js
@@ -1,6 +1,19 @@
-let arr = [10, 20, 30, 40, 50, 60, 70, 80];
-arr[3] = 800;
-arr.pop();
-arr.push(4000)
-let newarr = arr.slice(0, 4).concat(arr.slice(5));
-console.log(newarr);
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80];
+// arr[3] = 800;
+// arr.pop();
+// arr.push(4000)
+// arr.unshift(3000)
+// arr.shift()
+// let newarr = arr.slice(0, 4).concat(arr.slice(5));
+// console.log(newarr);
+
+
+//while loop
+
+let whiLe = 10;
+
+while(whiLe <= 15){
+    console.log(whiLe);
+    whiLe++;
+}
+
