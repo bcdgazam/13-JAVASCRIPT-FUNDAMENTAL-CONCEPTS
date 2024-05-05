35c7ff693f791998021ed81fe238f584978989fa
Golam Azam <bcdazam@gmail.com>
2024-05-05 16:31:08 +0600

    array practice

--- prac.js
+++ prac.js
@@ -10,10 +10,37 @@
 
 //while loop
 
-let whiLe = 10;
+// let whiLe = 10;
 
-while(whiLe <= 15){
-    console.log(whiLe);
-    whiLe++;
-}
+// while(whiLe <= 15){
+//     console.log(whiLe);
+//     whiLe++;
+// }
 
+
+//let's start array
+
+let arr = [10, 23, 45, 53, 5, 1, 23, 55, 67, 72]
+// console.log(arr)
+
+arr[2] = 900;
+console.log(arr)
+
+arr.push(30)
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
+
+let newArray = arr.slice(0, 2).concat(arr.slice(3))
+console.log(newArray)
+
+let getIndex = arr.indexOf(23)
+console.log(getIndex);
+
+arr.unshift(30);
+console.log(arr)
+
+arr.shift();
+console.log(arr)
