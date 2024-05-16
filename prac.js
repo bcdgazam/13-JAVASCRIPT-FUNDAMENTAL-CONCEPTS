7d2e2c9c76644fda2a331455e769fb4b7c4d41a8
Golam Azam <bcdazam@gmail.com>
2024-05-16 10:55:26 +0600

    while practice

--- prac.js
+++ prac.js
@@ -1,82 +1,10 @@
-// let arr = [10, 20, 30, 40, 50, 60, 70, 80];
-// arr[3] = 800;
-// arr.pop();
-// arr.push(4000)
-// arr.unshift(3000)
-// arr.shift()
-// let newarr = arr.slice(0, 4).concat(arr.slice(5));
-// console.log(newarr);
+// let arraY = [12, 34, 53, 56, 64, 10, 98, 900]
+// let myAge = arraY[3];
+// let result = arraY.slice(64);
+// console.log(myAge);
 
-
-//while loop
-
-// let whiLe = 10;
-
-// while(whiLe <= 15){
-//     console.log(whiLe);
-//     whiLe++;
-// }
-
-
-//let's start array
-
-// let arr = [10, 23, 45, 53, 5, 1, 23, 55, 67, 72]
-// console.log(arr)
-
-// arr[2] = 900;
-// console.log(arr)
-
-// arr.push(30)
-// console.log(arr)
-
-// arr.pop()
-// console.log(arr)
-
-
-// let newArray = arr.slice(0, 2).concat(arr.slice(3))
-// console.log(newArray)
-
-// let getIndex = arr.indexOf(23)
-// console.log(getIndex);
-
-// arr.unshift(30);
-// console.log(arr)
-
-// arr.shift();
-// console.log(arr)
-
-//while loop practice
-
-// let num = 1;
-// while(num <= 100){
-//     console.log(num);
-//     num++;
-// }
-
-//For loop practice
-
-// let nums = [10, 20, 30, 40, 50, 60, 80];
-// for(let i = 0; i < nums.length; i++){
-//     let element = nums[i];
-//     console.log(element);
-// }
-
-// switch case
-
-let num = 100;
-
-switch (num) {
-    case 50:
-        console.log('I am fifty')
-        break;
-    case 70:
-        console.log('I am Seventy');
-            break;
-    case 100:
-        console.log('I am hundred');
-        break;
-
-    default:
-        console.log('It is not matching')
-        break;
+let num = 10;
+while(num <= 15){
+    console.log(num);
+    num++;
 }
