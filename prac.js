2bc3c3db24524338bdba9eda09dce39c1be554ce
Golam Azam <bcdazam@gmail.com>
2024-05-16 10:56:28 +0600

    for loop practice

--- prac.js
+++ prac.js
@@ -3,8 +3,12 @@
 // let result = arraY.slice(64);
 // console.log(myAge);
 
-let num = 10;
-while(num <= 15){
-    console.log(num);
-    num++;
+// let num = 10;
+// while(num <= 15){
+//     console.log(num);
+//     num++;
+// }
+
+for(let i = 0; i <= 12; i++){
+    console.log(i);
 }
